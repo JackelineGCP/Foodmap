0931bfb2dffebd33a45a931bc74ed641355a335c
JackelineGCP <gessenia_17@hotmail.com>
2018-01-01 22:44:11 -0500

    agregando imagenes y funcionalidades en js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
   setTimeout(function() { 
-    window.location.href = '#';
-  }, 5000);
+    window.location.href = 'views/home.html';
+  }, 4000);
 });
+
+// Agregando nuestro mapa
+var map;
+function addMap() {
+  var coord = new google.maps.LatLng(47.6145, -122.3418); 
+  map = new google.maps.Map($('#map')[0], {
+    center: coord,
+    zoom: 10
+  });
+}
+
+function maps() {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      infoWindow = new google.maps.InfoWindow({ map: map });
+      var pos = { lat: position.coords.latitude,
+        lng: position.coords.longitude };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Found your location <br />Lat : ' + position.coords.latitude + ' </br>Lang :' + position.coords.longitude);
+      map.panTo(pos);
+    });
+  } else {
+    console.log('Browser doesn\'t support geolocation!');
+  }
+}
---
+++ js/data.js
@@ -0,0 +1,79 @@
+var restaurants = [
+  {
+    name: 'Polleria BEGUI',
+    type: 'Polleria',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/begui.png'
+  }, {
+    name: 'El Merlin',
+    type: 'Comida marina',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/elmerlin.jpg'
+  }, {
+    name: 'HIKARI',
+    type: 'Polleria',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/hikari.png'
+  }, {
+    name: 'La Leña',
+    type: 'Polleria',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/lalena.png',
+  }, {
+    name: 'La Pollerona',
+    type: 'Polleria',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/lapollerona.jpeg',
+  }, {
+    name: 'Mac Donalds',
+    type: 'Comida rapida',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/macdonalds.jpg',
+  }, {
+    name: 'Polleria Norkys',
+    type: 'Polleria',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/norkys.png',
+  }, {
+    name: 'Pardos Chiken',
+    type: 'Comida rapida',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/pardos.jpg',
+  }, {
+    name: 'Patty Wong',
+    type: 'Comida china',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/pattywong.jpeg',
+  }, {
+    name: 'Rikotón',
+    type: 'Polleria',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/rikoton.jpg',
+  }, {
+    name: 'Rokys',
+    type: 'Polleria',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/rokys.png',
+  }, {
+    name: 'Ronalds',
+    type: 'Polleria',
+    district: 'San juan de lurigancho',
+    route: 'assets/images/ronalds.jpg',
+  }, {
+    name: 'Rustica San Juan de Lurigancho',
+    type: 'Comida Peruana',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/rustica.jpeg',
+  }, {
+    name: 'Restaurante Sipan',
+    type: 'Polleria',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/sipan.jpg',
+  }, {
+    name: 'Restaurant UCHU',
+    type: 'Comida Peruana',
+    district: 'San Juan de Lurigancho',
+    route: 'assets/images/uchu.jpg',
+  }
+];
+  
